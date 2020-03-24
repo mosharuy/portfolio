caf563cc0f86a54ef233b713a7f8679583c04ce9
Radu Constantin <mosharuy@gmail.com>
2020-03-24 18:24:24 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+const main = {
+  name: 'Radu Constantin',
+  mail: 'mosharuy@gmail.com',
+  img: './profile.png',
+  role: ['JavaScript Enthusiast'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://raduconstantin.ro'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/mosharuy'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/radu-constantin-a1058bb0/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'mosharuy@gmail.com'}?Subject=Hello%20Radu`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blog', link: '#blogs' }
+  ]
+};
